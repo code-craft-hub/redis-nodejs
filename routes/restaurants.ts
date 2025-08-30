f7cec40f1307aef3e1135563973c59b50fb7df50
gabriel <onlinehassle1234@gmail.com>
2025-08-30 11:36:16 +0100

    Add Redis client initialization and key generation utility

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import { validate } from "../middleware/validate.js";
 import { Restaurant, RestaurantSchema } from "../schemas/restaurant.js";
+import { initializeRedisClient } from "../utils/client.js";
 
 export const restaurantRouter = Router();
 
@@ -9,6 +10,7 @@ restaurantRouter.post(
   validate(RestaurantSchema),
   async (req: Request, res: Response) => {
     const data = req.body as Restaurant;
+    const client = await initializeRedisClient();
     console.log(data);
     res.send("hello world");
   }
