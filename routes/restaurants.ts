ff51c79088aab7ac9769929da9164cbf25f0b3f7
gabriel <onlinehassle1234@gmail.com>
2025-08-30 11:25:47 +0100

    Implement restaurant validation middleware and schemas

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,7 +1,15 @@
 import { Router, Request, Response } from "express";
+import { validate } from "../middleware/validate.js";
+import { Restaurant, RestaurantSchema } from "../schemas/restaurant.js";
 
 export const restaurantRouter = Router();
 
-restaurantRouter.get("/", (req: Request, res: Response) => {
-    res.send("hello world")
-});
+restaurantRouter.post(
+  "/",
+  validate(RestaurantSchema),
+  async (req: Request, res: Response) => {
+    const data = req.body as Restaurant;
+    console.log(data);
+    res.send("hello world");
+  }
+);
